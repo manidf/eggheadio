46eceb1b333096641a79d34126f94b252b87ce0d
Mannuel Ferreira <mannuel@wwc.co.za>
2015-08-27 17:35:17 +0200

    componentDidMount and componentWillUnmount and componentWillMount functions

--- reactjs/component-lifecycle/app.jsx
+++ reactjs/component-lifecycle/app.jsx
@@ -9,10 +9,37 @@ var Button = React.createClass({
             val: this.state.val + 1
         });
     },
+    componentWillMount: function() {
+        console.log('mounting!');
+    },
     render: function() {
         console.log('rendering');
         return <button onClick={this.update}>{this.state.val}</button>
+    },
+    componentDidMount: function() {
+        console.log('mounted');
+    },
+    componentWillUnmount: function() {
+        console.log('bye!!!!');
     }
 });
 
-React.render(<Button/>, document.body);
+var App = React.createClass({
+    mount: function() {
+        React.render(<Button />, document.getElementById('app'));
+    },
+    unmount: function() {
+        React.unmountComponentAtNode(document.getElementById('app'));
+    },
+    render: function() {
+        return (
+            <div>
+                <button onClick={this.mount}>Mount</button>
+                <button onClick={this.unmount}>UnMount</button>
+                <div id="app"></div>
+            </div>
+        );
+    }
+});
+
+React.render(<App/>, document.body);
