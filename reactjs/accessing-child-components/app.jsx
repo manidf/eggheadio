96ae87ceb4860fc5ef9a3ca290e79c046854532e
Mannuel Ferreira <mannuel@wwc.co.za>
2015-09-10 23:24:12 +0200

    accesing child components

--- reactjs/accessing-child-components/app.jsx
+++ reactjs/accessing-child-components/app.jsx
@@ -1,11 +1,19 @@
 var App = React.createClass({
-
+    render: function() {
+        return <Button> <myIcon/> REACT </Button>
+    }
 });
 
-var Button = Reat.createClass({
-
+var Button = React.createClass({
+    render: function() {
+        return <button>{this.props.children}</button>
+    }
 });
 
 var myIcon = React.createClass({
-
+    render: function() {
+        return <span className="icon material-icons"></span>
+    }
 });
+
+React.render(<App />, document.getElementById('PageContent'));
